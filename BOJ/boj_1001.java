95eb01a92c8bba55f192066c9df7b292a0a403ed
chungy <yeop1438@naver.com>
2023-02-26 17:06:10 +0900

    1001_commit

--- BOJ/boj_1001.java
+++ BOJ/boj_1001.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class boj_1001 {
-    
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        scanner.close();
+        
+        System.out.println(a - b);
+    }
 }
